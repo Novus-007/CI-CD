c6cceabb537582088d0453adca008eedc34fec2e
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-08-17 17:56:43 +0530

    Function Added

---
+++ Function/Run_Pipline.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+df_Pipeline = pd.read_csv("Running_Pipline.csv")
+f = open("Running_Function.sh", "w")
+for i in df_Pipeline.index:
+	f.write("cd "+str(df_Pipeline["FileName"][i])+"\n")
+	f.write("python Test*.py\n")
+	f.write("coverage run --source=.  Test*.py\n")
+	f.write("coverage report \n")
+	f.write("coverage html \n")
+	f.write("cd ..\n")
+
+f.close()
